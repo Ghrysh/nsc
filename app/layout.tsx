ddf1666c85666d6a4f69e1106ba407ebf431ff5d
Ghrysh <gispi.desu@gmail.com>
2026-04-21 09:35:37 +0700

    toolbar preview

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import { VercelToolbar } from '@vercel/toolbar/next';
 
 export const metadata = {
   title: "Starlink",
@@ -11,11 +12,15 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const isPreview = process.env.VERCEL_ENV === 'preview' || process.env.NODE_ENV === 'development';
+
   return (
     <html lang="id" suppressHydrationWarning>
       <body className="bg-black text-white antialiased" suppressHydrationWarning>
         <Navbar />
         {children}
+
+        {isPreview && <VercelToolbar />}
       </body>
     </html>
   );
